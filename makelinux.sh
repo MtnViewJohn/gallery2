6c83ee0091e8210eec745f42e0a4b1943b2216fd
John Horigan <john@glyphic.com>
2018-11-22 13:26:57 -0800

    Add linux make script

---
+++ makelinux.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+set -e
+
+echo "Compiling..."
+
+mkdir build 2>/dev/null || true
+elm make src/Main.elm --output build/main.js
+
+# concat JS together
+# cat assets/*.js build/main-elm.js > build/main.js
+
+use_java=true
+
+closure_jar=~/bin/closure-compiler-v20180402.jar
+
+echo "Minifying..."
+
+if $use_java
+then
+  java -jar "$closure_jar" \
+  --js build/main.js \
+  --js_output_file build/main-min.js \
+  --create_source_map build/main.map \
+  --jscomp_off uselessCode
+# cp build/main.js build/main-min.js
+else
+  curl --output build/main-min.js \
+    --data output_info=errors \
+    --data output_info=compiled_code \
+    --data-urlencode 'js_code@build/main.js' \
+    https://closure-compiler.appspot.com/compile
+fi
+
+
+echo "Compressing..."
+
+gzip -9 <build/main-min.js >/var/www/cfa/gallery2/build/main.js.gz
+
+echo "Done!"
+
+# rm -rf distribution
+
+# appdir=distribution/elk-herd
+# mkdir -p "$appdir"
+# cp -rp assets "$appdir"
+# rm "$appdir/assets/"*.js    # already incorporated into the main.js file
+# cp -p build/main-min.js "$appdir/assets/main.js"
+# cp -p index.html "$appdir"
+
+# (cd distribution; \
+#  tar cvzf elk-herd-live.tgz elk-herd \
+# )
+
+# wget -E -H -k -K -nd -N -p -P thepageslug \
+#      http://www.thepage.to/save/four/posterity.html
+#
+# see: https://gist.github.com/dannguyen/03a10e850656577cfb57
+
+
